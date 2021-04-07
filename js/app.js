9715341306800f15144dd88814182783c03e043e
amroalbarham <amroalbarham@gmail.com>
2021-04-07 21:04:37 +0300

    still not working

--- js/app.js
+++ js/app.js
@@ -10,6 +10,8 @@ let timeOfcookie = ['6:00am', '7:00am', '8:00am', '9:00am', '10:00am', '11:00am'
 let parent = document.getElementById('parent');
 let tableelemnts = document.createElement('table');
 parent.appendChild(tableelemnts);
+tableelemnts.setAttribute('id', 'table-1');
+
 
 
 function showTable() {
@@ -27,7 +29,7 @@ function showTable() {
   tableelemnts.appendChild(thelastHeading);
   thelastHeading.textContent = 'Daily Location Total';
 
-//heloo
+  //heloo
 }
 showTable();
 
@@ -70,7 +72,7 @@ Location.prototype.render = function () {
   tdelemnts.textContent = this.goodname;
 
   let sum = 0;
- 
+
 
   for (let i = 0; i < timeOfcookie.length; i++) {
     let tdelemnts = document.createElement('td');
@@ -79,8 +81,8 @@ Location.prototype.render = function () {
     sum += this.randomNumber1[i];
   }
   // sumone+=sum; 
- 
- 
+
+
   let last = document.createElement('td');
   trelemnts.appendChild(last);
   last.textContent = sum;
@@ -89,6 +91,8 @@ Location.prototype.render = function () {
 
 
 }
+
+
 let Seattle = new Location('seattle', 6.3, 23, 65);
 let Tokyo = new Location('Tokyo', 1.2, 3, 24);
 let Dubai = new Location('Dubai', 3.7, 11, 38);
@@ -97,321 +101,82 @@ let lima = new Location('lima', 4.6, 2, 16);
 for (let l = 0; l < locations.length; l++) {
   locations[l].render();
 }
-let totalone=0;
-let total=0;
-let trelemnts=document.createElement('tr');
-tableelemnts.appendChild(trelemnts);
-let thelemnts=document.createElement('th');
-trelemnts.appendChild(thelemnts);
-thelemnts.textContent='Totals'; 
-
-for(let f=0;f<timeOfcookie.length;f++){
-      for(let m=0;m<locations.length;m++){
-        total+=locations[m].randomNumber1[f];
-        totalone+=locations[m].randomNumber1[f];
-      
-        
-      }
-      console.log(total);
-      let tdelemnts=document.createElement('th');
-      trelemnts.appendChild(tdelemnts);
-      tdelemnts.textContent=total;
-      // totalone+=total;  
-      total=0;
 
 
+
+
+let cookieLocation = document.getElementById('locationsForm');
+
+console.log(cookieLocation);
+cookieLocation.addEventListener('submit', submitter);
+
+function submitter(event) {
+  event.preventDefault();
+
+
+  let name = event.target.nameOfLocations.value;
+  console.log(name);
+  let avarage = event.target.avaregeCookei.value;
+  console.log('avs', avarage);
+
+  let minimum = event.target.minmumCustumer.value;
+  let maximum = event.target.maximumCustmer.value;
+
+  let newLocation = new Location(name, avarage, minimum, maximum);
+
+  let amro = document.getElementById('locationsForm');
+  // amro.removeChild(amro.lastChild);
+  newLocation.render();
+  footer();
+  // amro.textContent=' ';
+
+
+  // console.log('asdfasdsf');
+
+
+
+  // newLocation.render(); 
 }
+// newLocation.render();
+
+// submitter();
+
+
+
+let total = 0;
+let totalone = 0;
+function footer() {
+
+  let trelemnts = document.createElement('tr');
+  tableelemnts.appendChild(trelemnts);
+  let thelemnts = document.createElement('th');
+  trelemnts.appendChild(thelemnts);
+  thelemnts.textContent = 'Totals';
+
+  for (let f = 0; f < timeOfcookie.length; f++) {
+
+    for (let m = 0; m < locations.length; m++) {
+      total += locations[m].randomNumber1[f];
+      totalone += locations[m].randomNumber1[f];
+    }
+
+    // console.log(total);
+    let tdelemnts = document.createElement('th');
+    trelemnts.appendChild(tdelemnts);
+    tdelemnts.textContent = total;
+    // totalone+=total;  
+    total = 0;
+  }
+  let lastLast = document.createElement('th');
+  trelemnts.appendChild(lastLast);
+  lastLast.textContent = totalone;
+}
+// footer();
+
+
+
+
 // console.log(sumone);
-console.log(totalone);
+// console.log(totalone);
 
-let lastLast=document.createElement('th');
-trelemnts.appendChild(lastLast);
-lastLast.textContent=totalone;
 
-// let finalSumrow=0;
-// for(let r=0;r<sumone.length;r++){
-//   finalSumrow+=sumone[r];}
-
-// let totalfinalcoloum=0;       
-// for(let w=0;w<totalone.length;w++){
-//   totalfinalcoloum+=totalone[w]};  
-
-// console.log(finalSumrow);
-// console.log(totalfinalcoloum);
-// let TotalTotal=totalfinalcoloum+finalSumrow;
-// console.log(TotalTotal);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// let Seattle = {
-
-//   goodname: 'Seattle',
-//   randomNumber1: [],
-
-//   aavarage: 6.3,
-//   minimum: 23,
-//   maximum: 65,
-//   coutmerperhour: 0,
-
-
-//   gitCoustmer: function () {
-//     for (let i = 0; i < timeOfcookie.length; i++) {
-
-//       this.randomNumber1.push(Math.floor(randomNumber(this.maximum, this.minimum) * this.aavarage));
-//       console.log(this.randomNumber1);
-//        }},
-
-
-//   render: function () {
-
-
-//     let parent = document.getElementById('parent');  
-//     let nameOflocation=document.createElement('h3');
-//     parent.appendChild(nameOflocation);
-//     nameOflocation.textContent=this.goodname;
-
-//     let unelemnt = document.createElement('ul');
-//     parent.appendChild(unelemnt);
-//     console.log(parent);
-//     let sum =0;
-
-
-
-
-//     for (let j = 0; j<timeOfcookie.length; j++) {
-
-//       let lielemnt = document.createElement('li');
-//       unelemnt.appendChild(lielemnt);
-//       lielemnt.textContent = timeOfcookie[j] + ' : ' + this.randomNumber1[j] +  ' cookies';
-//       console.log(lielemnt.textContent);
-//       sum+=this.randomNumber1[j];
-//       }
-
-
-//     let lielemnt=document.createElement('li');
-//     lielemnt.textContent ='total '+ sum+' cookeis';
-//     unelemnt.appendChild(lielemnt);
-//    }
-//   };
-// Seattle.gitCoustmer();
-// console.log(Seattle.randomNumber1);
-// console.log('hello dana');
-// Seattle.render();
-
-
-// let Tokyo = {
-
-//   goodname: 'Tokyo',
-//   randomNumber1: [],
-//   aavarage: 1.2,
-//   minimum: 3,
-//   maximum: 24,
-//   coutmerperhour: 0,
-
-
-//   gitCoustmer: function () {
-//     for (let i = 0; i < timeOfcookie.length; i++) {
-
-//       this.randomNumber1.push(Math.floor(randomNumber(this.maximum, this.minimum) * this.aavarage));
-//       console.log(this.randomNumber1);
-//        }},
-
-
-//   render: function () {
-
-//     let parent = document.getElementById('parent');  
-//     let nameOflocation=document.createElement('h3');
-//     parent.appendChild(nameOflocation);
-//     nameOflocation.textContent=this.goodname;
-//     let unelemnt = document.createElement('ul');
-//     parent.appendChild(unelemnt);
-//     console.log(parent);
-//     let sum =0;
-
-
-
-
-//     for (let j = 0; j<timeOfcookie.length; j++) {
-
-//       let lielemnt = document.createElement('li');
-//       unelemnt.appendChild(lielemnt);
-//       lielemnt.textContent = timeOfcookie[j] + ' : ' + this.randomNumber1[j] +  ' cookies';
-//       console.log(lielemnt.textContent);
-//       sum+=this.randomNumber1[j];
-//       }
-
-
-//     let lielemnt=document.createElement('li');
-//     lielemnt.textContent ='total '+ sum+' cookeis';
-//     unelemnt.appendChild(lielemnt);
-//    }
-//   };
-
-// Tokyo.gitCoustmer();
-// console.log(Tokyo.randomNumber1);
-// Tokyo.render();
-
-
-
-// let Dubai = {
-
-//   goodname: 'Dubai',
-//   randomNumber1: [],
-//   aavarage: 3.7,
-//   minimum: 11,
-//   maximum: 38,
-//   coutmerperhour: 0,
-
-
-//   gitCoustmer: function () {
-//     for (let i = 0; i < timeOfcookie.length; i++) {
-
-//       this.randomNumber1.push(Math.floor(randomNumber(this.maximum, this.minimum) * this.aavarage));
-//       console.log(this.randomNumber1);
-//        }},
-
-
-//   render: function () {
-
-//     let parent = document.getElementById('parent');  
-//     let nameOflocation=document.createElement('h3');
-//     parent.appendChild(nameOflocation);
-//     nameOflocation.textContent=this.goodname;
-//     let unelemnt = document.createElement('ul');
-//     parent.appendChild(unelemnt);
-//     console.log(parent);
-//     let sum =0;
-
-
-//     for (let j = 0; j<timeOfcookie.length; j++) {
-
-//       let lielemnt = document.createElement('li');
-//       unelemnt.appendChild(lielemnt);
-//       lielemnt.textContent = timeOfcookie[j] + ' : ' + this.randomNumber1[j] +  ' cookies';
-//       console.log(lielemnt.textContent);
-//       sum+=this.randomNumber1[j];
-//       }
-
-
-//     let lielemnt=document.createElement('li');
-//     lielemnt.textContent ='total '+ sum+' cookeis';
-//     unelemnt.appendChild(lielemnt);
-//    }
-//   };
-
-//   Dubai.gitCoustmer();
-// console.log(Dubai.randomNumber1);
-// Dubai.render();
-
-
-// let Paris = {
-
-//   goodname: 'Paris',
-//   randomNumber1: [],
-//   aavarage: 2.3,
-//   minimum: 20,
-//   maximum: 38,
-//   coutmerperhour: 0,
-
-
-//   gitCoustmer: function () {
-//     for (let i = 0; i < timeOfcookie.length; i++) {
-
-//       this.randomNumber1.push(Math.floor(randomNumber(this.maximum, this.minimum) * this.aavarage));
-//       console.log(this.randomNumber1);
-//        }},
-
-
-//   render: function () {
-
-//     let parent = document.getElementById('parent');  
-//     let nameOflocation=document.createElement('h3');
-//     parent.appendChild(nameOflocation);
-//     nameOflocation.textContent=this.goodname;
-//     let unelemnt = document.createElement('ul');
-//     parent.appendChild(unelemnt);
-//     console.log(parent);
-//     let sum =0;
-
-
-//     for (let j = 0; j<timeOfcookie.length; j++) {
-
-//       let lielemnt = document.createElement('li');
-//       unelemnt.appendChild(lielemnt);
-//       lielemnt.textContent = timeOfcookie[j] + ' : ' + this.randomNumber1[j] +  ' cookies';
-//       console.log(lielemnt.textContent);
-//       sum+=this.randomNumber1[j];
-//       }
-
-
-//     let lielemnt=document.createElement('li');
-//     lielemnt.textContent ='total '+ sum+' cookeis';
-//     unelemnt.appendChild(lielemnt);
-//    }
-//   };
-
-// Paris.gitCoustmer();
-// console.log(Paris.randomNumber1);
-// Paris.render();
-
-// let Lima = {
-
-//   goodname: 'Lima',
-//   randomNumber1: [],
-//   aavarage: 2.3,
-//   minimum: 20,
-//   maximum: 38,
-//   coutmerperhour: 0,
-
-
-//   gitCoustmer: function () {
-//     for (let i = 0; i < timeOfcookie.length; i++) {
-
-//       this.randomNumber1.push(Math.floor(randomNumber(this.maximum, this.minimum) * this.aavarage));
-//       console.log(this.randomNumber1);
-//        }},
-
-
-//   render: function () {
-
-//     let parent = document.getElementById('parent');  
-//     let nameOflocation=document.createElement('h3');
-//     parent.appendChild(nameOflocation);
-//     nameOflocation.textContent=this.goodname;
-//     let unelemnt = document.createElement('ul');
-//     parent.appendChild(unelemnt);
-//     console.log(parent);
-//     let sum =0;
-
-
-//     for (let j = 0; j<timeOfcookie.length; j++) {
-
-//       let lielemnt = document.createElement('li');
-//       unelemnt.appendChild(lielemnt);
-//       lielemnt.textContent = timeOfcookie[j] + ' : ' + this.randomNumber1[j] +  ' cookies';
-//       console.log(lielemnt.textContent);
-//       sum+=this.randomNumber1[j];
-//       }
-
-
-//     let lielemnt=document.createElement('li');
-//     lielemnt.textContent ='total '+ sum+' cookeis';
-//     unelemnt.appendChild(lielemnt);
-//    }
-//   };
-
-
-// Lima.gitCoustmer();
-// console.log(Lima.randomNumber1);
-// Lima.render();
